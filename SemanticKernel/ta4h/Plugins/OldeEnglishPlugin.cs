94f5768d91fa15e456a9e1d9224df2fb7e34266d
Matthew C. Anderson <matt@matthewcanderson.com>
2023-10-12 20:29:48 -0500

    copy of OE

--- SemanticKernel/ta4h/Plugins/OldeEnglishPlugin.cs
+++ SemanticKernel/ta4h/Plugins/OldeEnglishPlugin.cs
@@ -2,11 +2,6 @@ using Microsoft.SemanticKernel.SkillDefinition;
 
 public class OldeEnglishPlugin
 {
-    /// <summary>
-    /// Given a word or phrase, translate it into Olde English
-    /// </summary>
-    /// <param name="input">The word or phrase to translate</param>
-    /// <returns>The translated word or phrase</returns>
     [SKFunction()]
     public string Translate(string input)
     {
